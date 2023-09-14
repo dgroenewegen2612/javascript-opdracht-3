1957df7bffbecf997f6e86c9acb963b40a37bf84
dgroenewegen2612 <302782703@student.rocmondriaan.nl>
2023-09-14 14:44:50 +0200

    javascript opdracht 3 verdergewerkt

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 console.log('main.js runt')
 
+//opdracht 1
 let elementH1=document.querySelector('h1');
 
-elementH1.textContent = '';
+elementH1.textContent = 'veranderd';
 
+
+
+//opdracht 2
 let changebutton = document.querySelector('.knop');
-
 changebutton.addEventListener('click',function(){
    let result= document.querySelector('.result');
     result.textContent='it works!';
 })
+
+//opdracht 3
+
+let userImput= prompt("type your name");
+let div=document.querySelector('.own-text');
+div.textContent=userImput;
+
